327a1025acca34e35c7677b17418170d26340202
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2020-07-14 17:47:44 +0530

    Update getClientSystemTimeZone.php

--- getClientSystemTimeZone.php
+++ getClientSystemTimeZone.php
@@ -6,3 +6,8 @@ a = parseFloat(a);
 
 $timezone = 7;
 echo gmdate("Y/m/j H:i:s", time() + 3600*($timezone+date("I")));
+
+
+-------------------------------------
+This give the timezone name like " Asia/Calcutta "
+Intl.DateTimeFormat().resolvedOptions().timeZone
