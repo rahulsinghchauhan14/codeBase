3e923cf0e4f2d00e00ce11936b94962aff4bc61a
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2020-07-14 14:32:27 +0700

    Create getClientSystemTimeZone.php

---
+++ getClientSystemTimeZone.php
@@ -0,0 +1,8 @@
+var visitortime = new Date();
+var visitortimezone = "GMT " + -visitortime.getTimezoneOffset()/60;
+var a = "GMT 6.5".replace ( /[^\d.]/g, '' );
+a = parseFloat(a);
+
+
+$timezone = 7;
+echo gmdate("Y/m/j H:i:s", time() + 3600*($timezone+date("I")));
